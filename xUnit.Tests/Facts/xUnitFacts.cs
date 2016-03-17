8fa127e250a9d0041654772f2cd91503553c20fa
estebanvargas <esteban.vargas@trintech.com>
2016-03-17 09:29:18 -0600

    adding back failing data for tests

--- xUnit.Tests/Facts/xUnitFacts.cs
+++ xUnit.Tests/Facts/xUnitFacts.cs
@@ -7,6 +7,7 @@ namespace xUnit.Tests.Facts
         [Theory]
         [InlineData(3)]
         [InlineData(5)]
+        [InlineData(6)]
         public void MyFirstTheory(int value)
         {
             Assert.True(IsOdd(value));
